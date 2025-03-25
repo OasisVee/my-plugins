0d160b95a06128e2a113c1dc520dcf06d3f0363d
SeriNova <131408227+OasisVee@users.noreply.github.com>
2025-03-24 20:44:17 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Plugins
+
+[sed](https://gitlab.com/Grzesiek11/sed-aliucord-plugin) was made by Grzesiek11, this is just a port of it to github since i couldnt get gitlab to build
---
+++ Sed/build.gradle.kts
@@ -0,0 +1,9 @@
+version = "1.1.0"
+description = "Allows editing messages with a sed-like substitiution command"
+aliucord.author("Grzesiek11", 368475654662127616L)
+aliucord.changelog.set(
+  """
+  # 1.1.0
+  * made the initial s case-insensitive
+  """.trimIndent()
+ )
